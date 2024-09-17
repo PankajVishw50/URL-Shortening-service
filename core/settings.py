459a9e8c837ff5f1345ec20c10922a20a211714a
Pankaj <vishw.dev.1000@gmail.com>
2024-09-17 19:28:00 +0530

    Initialized Postgres as default database
    Initialzed postgres as default db
    Setted up .env for project

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DATABASE_NAME'),
+        'HOST': os.environ.get('POSTGRES_DATABASE_HOST'),
+        'PORT': os.environ.get('POSTGRES_DATABASE_PORT'),
+        'USER': os.environ.get('POSTGRES_DATABASE_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_DATABASE_PASSWORD'),
     }
 }
 
